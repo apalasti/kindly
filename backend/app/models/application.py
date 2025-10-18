0607131f2011fc26990bafca9035a449fbde3675
Andras Palasti <andraspalasti2@gmail.com>
2025-10-18 20:17:26 +0200

    triggers for avg_rating update

--- backend/app/models/application.py
+++ backend/app/models/application.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import TIMESTAMP, Boolean, ForeignKey, Integer, String, text
+from sqlalchemy import DDL, TIMESTAMP, ForeignKey, Integer, String, event, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql.schema import UniqueConstraint
 
@@ -33,3 +33,67 @@ class Application(Base):
 
     volunteer: Mapped["User"] = relationship("User", viewonly=True)
     request: Mapped["Request"] = relationship("Request", viewonly=True)
+
+
+update_help_seeker_func = DDL("""
+CREATE OR REPLACE FUNCTION update_help_seeker_avg_rating_func()
+RETURNS TRIGGER AS $$
+DECLARE
+    help_seeker_id INT;
+BEGIN
+    IF (OLD.help_seeker_rating IS DISTINCT FROM NEW.help_seeker_rating) THEN
+        -- Get the creator_id (help seeker's ID) from the request table
+        SELECT creator_id INTO help_seeker_id
+        FROM request
+        WHERE id = NEW.request_id;
+
+        UPDATE "user"
+        SET avg_rating = (
+            SELECT COALESCE(AVG(a.help_seeker_rating), 0)
+            FROM application a
+            JOIN request r ON a.request_id = r.id
+            WHERE r.creator_id = help_seeker_id AND a.help_seeker_rating IS NOT NULL
+        )
+        WHERE id = help_seeker_id;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+""")
+
+update_help_seeker_trigger = DDL("""
+CREATE OR REPLACE TRIGGER update_avg_help_seeker_rating
+AFTER UPDATE OF help_seeker_rating ON application
+FOR EACH ROW
+EXECUTE FUNCTION update_help_seeker_avg_rating_func();
+""")
+
+update_volunteer_func = DDL("""
+CREATE OR REPLACE FUNCTION update_volunteer_avg_rating_func()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (OLD.volunteer_rating IS DISTINCT FROM NEW.volunteer_rating) THEN
+        UPDATE "user"
+        SET avg_rating = (
+            SELECT COALESCE(AVG(a.volunteer_rating), 0)
+            FROM application a
+            WHERE a.user_id = NEW.user_id AND a.volunteer_rating IS NOT NULL
+        )
+        WHERE id = NEW.user_id;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+""")
+
+update_volunteer_trigger = DDL("""
+CREATE OR REPLACE TRIGGER update_avg_volunteer_rating
+AFTER UPDATE OF volunteer_rating ON application
+FOR EACH ROW
+EXECUTE FUNCTION update_volunteer_avg_rating_func();
+""")
+
+event.listen(Application.__table__, "after_create", update_help_seeker_func.execute_if(dialect="postgresql"))
+event.listen(Application.__table__, "after_create", update_help_seeker_trigger.execute_if(dialect="postgresql"))
+event.listen(Application.__table__, "after_create", update_volunteer_func.execute_if(dialect="postgresql"))
+event.listen(Application.__table__, "after_create", update_volunteer_trigger.execute_if(dialect="postgresql"))
