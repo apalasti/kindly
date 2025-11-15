52e8b148909c944f577a5de6d9b63e8d96389bcd
Andras Palasti <andraspalasti2@gmail.com>
2025-11-15 20:23:24 +0100

    Implement retries for db connection

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 from dotenv import load_dotenv
@@ -20,17 +21,25 @@ engine = create_async_engine(
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        our_table_names = set(Base.metadata.tables.keys())
-        existing_tables = await conn.run_sync(
-            lambda sync_conn: [
-                t for t in inspect(sync_conn).get_table_names()
-                if t in our_table_names
-            ]
-        )
-        await conn.run_sync(Base.metadata.create_all)
-        return len(existing_tables) != 0
+async def create_db_and_tables(max_retries=5, retry_delay=5):
+    attempt = 0
+    while attempt < max_retries:
+        try:
+            async with engine.begin() as conn:
+                our_table_names = set(Base.metadata.tables.keys())
+                existing_tables = await conn.run_sync(
+                    lambda sync_conn: [
+                        t for t in inspect(sync_conn).get_table_names()
+                        if t in our_table_names
+                    ]
+                )
+                await conn.run_sync(Base.metadata.create_all)
+                return len(existing_tables) != 0
+        except ConnectionRefusedError:
+            attempt += 1
+            if attempt >= max_retries:
+                raise
+            await asyncio.sleep(retry_delay)
 
 
 async def get_session():
